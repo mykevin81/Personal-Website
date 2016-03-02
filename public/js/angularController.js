cee33009253e51b52cbd4d27741b8366e6f58c0d
mykevin81 <mykevin81@gmail.com>
2016-03-01 22:22:36 -0800

    add single page app routeProvider for angular create partial ejs for main, project and projectList

--- public/js/angularController.js
+++ public/js/angularController.js
@@ -2,3 +2,28 @@
  * Created by mykev on 3/1/2016.
  */
 // TODO create angular controller using this file. Create factory to generate Project data
+
+var app = angular.module('personalApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+
+    $routeProvider
+
+        //load home page
+        .when('/', {
+            templateUrl: 'main.ejs',
+            controller: 'mainController'
+        })
+
+        //load projectList page
+        .when('/projectList', {
+            templateUrl: 'projectList.ejs',
+            controller: 'projectListController'
+        })
+
+        //load project page
+        .when('/project', {
+            templateUrl: 'project.ejs',
+            controller: 'projectController'
+        });
+});
