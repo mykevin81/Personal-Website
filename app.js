3e1d9548919b8eeac5e062852c0932f537d248e0
mykevin81 <mykevin81@gmail.com>
2016-05-01 17:29:04 -0700

    add cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 /*passport.use(new GoogleStrategy({
     clientID: 556527221886-1gov53rrlcidvkqjiuuffe8gvet5mqvo.apps.googleusercontent.com,
     clientSecret: zTaO8qmwJuVPWw6LdnrSYITF,
